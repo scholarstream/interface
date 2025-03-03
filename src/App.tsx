4fcb941f81da7b851ad2275e7464a544f0dae1f8
Dimas <dimasriat@gmail.com>
2025-03-03 20:06:40 +0700

    feat: add connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 function App() {
   return (
     <div>
       {/* Header & Wallet */}
       <h1>🎓 ScholarStream</h1>
-      <button>Connect Wallet</button>
-      <p>Wallet: 0x1234...abcd <button>Disconnect</button></p>
+      <ConnectButton/>
 
       {/* Balances Section */}
       <h2>📊 Balances</h2>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,48 @@
-import { StrictMode } from 'react'
+import { FC, ReactNode, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const RainbowkitWrapper: FC<{ children?: ReactNode }> = ({ children }) => (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RainbowkitWrapper>
+      <App />
+    </RainbowkitWrapper>
   </StrictMode>,
 )
